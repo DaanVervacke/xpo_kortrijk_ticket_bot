fd283dc5a8ff501c8723c5da0ec398fff2abece0
DaanVervacke <daan.vervacke@gmail.com>
2023-06-06 22:38:01 +0200

    new qr code screen + /ticket handler

--- bot.py
+++ bot.py
@@ -6,6 +6,10 @@ from io import BytesIO
 
 from dotenv import load_dotenv
 
+from PIL import Image
+from PIL import ImageDraw
+from PIL import ImageFont
+
 from datetime import datetime as dt
 
 from aiogram import Bot, Dispatcher, executor, types
@@ -13,9 +17,9 @@ from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
 
 # ENV stuff
 load_dotenv()
-API_TOKEN = os.getenv('API_TOKEN') # Your own Telegram bot API token
-BASE = os.getenv('BASE') # 540513002 (for now)
-END = os.getenv('END') # 515 (for now)
+API_TOKEN = os.getenv('API_TOKEN')  # Your own Telegram bot API token
+BASE = os.getenv('BASE')  # 540513002 (for now)
+END = os.getenv('END')  # 515 (for now)
 
 # Configure logging
 logging.basicConfig(level=logging.INFO)
@@ -26,9 +30,12 @@ dp = Dispatcher(bot)
 
 # Create custom keyboard
 ticket_button = KeyboardButton('🎟️  Genereer parkeerticket  🎟️')
-bot_kb = ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard=True).add(ticket_button)
+bot_kb = ReplyKeyboardMarkup(
+    resize_keyboard=True, one_time_keyboard=True).add(ticket_button)
 
 # Wait for start command and show keyboard
+
+
 @dp.message_handler(commands=['start'])
 async def send_welcome(message: types.Message):
     """
@@ -37,6 +44,8 @@ async def send_welcome(message: types.Message):
     await message.reply("Hallo!\nKlaar om gratis te parkeren?!\n", reply_markup=bot_kb)
 
 # Wait for ticket command, create ticket and respond with qr
+
+@dp.message_handler(commands=['ticket'])
 @dp.message_handler(regexp='Genereer parkeerticket')
 async def english(message: types.Message):
 
@@ -53,31 +62,53 @@ async def english(message: types.Message):
 
     # Create code with base + time + end
     code = f'{BASE}{year}{month}{day}{hour}{minute}{second}{END}'
-    
+
     # Create caption
-    caption = f"Code: {code}\nScan de QR aan de uitgang van de parking!"
-    
+    caption = f"Code: {code}\n\nScan de QR aan de uitgang van de parking!\n\n\nt.me/XpoKortrijkTicketBot"
+
     # Create QR
     qr = qrcode.QRCode(
-    version=1,
-    error_correction=qrcode.constants.ERROR_CORRECT_M,
-    border=13
+        version=1,
+        error_correction=qrcode.constants.ERROR_CORRECT_M,
+        border=0
     )
-    
+
     # Add data to QR
     qr.add_data(code)
 
     # Create image from QR
-    img = qr.make_image(fill_color="black", back_color="white")
+    qr_code = qr.make_image(
+        fill_color="black", back_color="white").resize((500, 500))
+
+    # Create white background
+    background = Image.new("RGB", (1080, 1920), (255, 255, 255))
+
+    # Load double arrow
+    arrow = Image.open("arrow_up.png").resize((100, 100))
+
+    # Paste qr code on background
+    background.paste(qr_code, (290, 85))
+
+    # Paste double arrow on background
+    background.paste(arrow, (490, 910), mask=arrow)
+
+    # Create font from .tff file
+    font = ImageFont.truetype("open_sans.ttf", 45)
+
+    # Create drawing context
+    context = ImageDraw.Draw(background)
+
+    context.multiline_text(xy=(540, 1500), text=caption, font=font, fill=(
+        0, 0, 0), anchor="ms", align="center")
 
     # Convert to Bytes
     bio = BytesIO()
     bio.name = "qr.png"
-    img.save(bio, 'PNG')
+    background.save(bio, 'PNG')
     bio.seek(0)
-    
+
     # Send
-    await message.reply_photo(photo=bio, caption=caption)
+    await message.reply_photo(photo=bio)
 
 # Run bot
 if __name__ == '__main__':
